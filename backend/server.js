4f2244ef233d20f8916f0958f77f1da5ea97359f
Sandy <kumarcse883@gmail.com>
2026-04-21 23:46:04 +0530

    vercel deployment small changes

--- backend/server.js
+++ backend/server.js
@@ -596,6 +596,4 @@ app.patch('/api/seating/:studentId', async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
